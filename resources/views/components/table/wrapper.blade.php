b47f134f81051170ddd806f57f1ce62871ce52de
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-02-28 13:08:29 +0700

    add basic table components

---
+++ resources/views/components/table/wrapper.blade.php
@@ -0,0 +1,10 @@
+<table class="divide-y divide-cool-gray-200">
+    <thead>
+        <tr>
+            {{ $head }}
+        </tr>
+    </thead>
+    <tbody class="divide-y divide-cool-gray-200">
+        {{ $body }}
+    </tbody>
+</table>
