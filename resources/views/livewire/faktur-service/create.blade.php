cafcbb1df0c2bbd6e963179f3e60741deb963099
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-03-18 12:33:49 +0700

    fix faktur create

--- resources/views/livewire/faktur-service/create.blade.php
+++ resources/views/livewire/faktur-service/create.blade.php
@@ -67,65 +67,66 @@
         </div>
         <hr class="my-4">
         {{-- PENJUALAN SERVIS --}}
+        @if ($selectedService->hasAnyPenjualanServices())
         <div class="">
             <h3 class="font-semibold text-lg uppercase mb-4">Penjualan Servis</h3>
-            
-            <div class="mb-4">
-                <div class="border-b-2 border-primary grid grid-cols-12 py-2 font-semibold gap-4">
-                    <div class="col-span-4">Jenis Service</div>
-                    <div class="col-span-2">Harga</div>
-                    <div class="col-span-2">Jumlah</div>
-                    <div class="col-span-4">Subtotal</div>
+            <div class="mb-4 border-l border-r border-primary" >
+                <div class="bg-primary text-white grid grid-cols-12 py-2 font-semibold gap-4">
+                    <div class="col-span-4 px-4">Jenis Service</div>
+                    <div class="col-span-2 px-4">Harga</div>
+                    <div class="col-span-2 px-4">Jumlah</div>
+                    <div class="col-span-4 px-4">Subtotal</div>
                 </div>
                 @foreach ($selectedService->penjualan_services as $penjualanService)
                     <div class="border-b border-primary grid grid-cols-12 py-2 gap-4">
-                        <div class="col-span-4">{{$penjualanService->jenis_service->nama}}</div>
-                        <div class="col-span-2">{{$penjualanService->harga}}</div>
-                        <div class="col-span-2">{{$penjualanService->jumlah}}</div>
-                        <div class="col-span-4">{{$penjualanService->getTotal()}}</div>
+                        <div class="col-span-4 px-4">{{$penjualanService->jenis_service->nama}}</div>
+                        <div class="col-span-2 px-4">{{$penjualanService->harga}}</div>
+                        <div class="col-span-2 px-4">{{$penjualanService->jumlah}}</div>
+                        <div class="col-span-4 px-4">{{$penjualanService->getTotal()}}</div>
                     </div>
                 @endforeach
             </div>
 
             <div class="grid grid-cols-12 py-2 gap-4">
-                <div class="col-span-9 col-start-5 border-t-4 border-primary"></div>
-                <div class="col-span-4 col-start-5 uppercase font-bold text-xl">TOTAL SERVICE</div>
-                <div class="col-span-4 col-start-9 text-xl">{{ $selectedService->getTotalPenjualanServices()}}</div>
+                <div class="col-span-4 col-start-5 uppercase font-bold pl-4">TOTAL SERVICE</div>
+                <div class="col-span-4 col-start-9 pl-4">{{ $selectedService->getTotalPenjualanServices()}}</div>
             </div>
         </div>
         <hr class="my-4">
+        @endif
         {{-- SUKU CADANG --}}
-        <div>
+        @if ($selectedService->hasAnyPenggantianSukuCadangs())
+        <div class="">
             <h3 class="font-semibold text-lg uppercase mb-4">Penggantian Suku Cadang</h3>
-            <div class="mb-4">
-                <div class="border-b-2 border-primary grid grid-cols-12 py-2 font-semibold gap-4">
-                    <div class="col-span-4">Suku Cadang</div>
-                    <div class="col-span-2">Harga</div>
-                    <div class="col-span-2">Jumlah</div>
-                    <div class="col-span-4">Subtotal</div>
+            <div class="mb-4 border-l border-r border-primary" >
+                <div class="bg-primary text-white grid grid-cols-12 py-2 font-semibold gap-4">
+                    <div class="col-span-4 px-4">Suku Cadang</div>
+                    <div class="col-span-2 px-4">Harga</div>
+                    <div class="col-span-2 px-4">Jumlah</div>
+                    <div class="col-span-4 px-4">Subtotal</div>
                 </div>
                 @foreach ($selectedService->penggantian_suku_cadangs as $penggantianSukuCadang)
                     <div class="border-b border-primary grid grid-cols-12 py-2 gap-4">
-                        <div class="col-span-4">{{$penggantianSukuCadang->suku_cadang->nama}}</div>
-                        <div class="col-span-2">{{$penggantianSukuCadang->harga}}</div>
-                        <div class="col-span-2">{{$penggantianSukuCadang->jumlah}}</div>
-                        <div class="col-span-4">{{$penggantianSukuCadang->getTotal()}}</div>
+                        <div class="col-span-4 px-4">{{$penggantianSukuCadang->suku_cadang->nama}}</div>
+                        <div class="col-span-2 px-4">{{$penggantianSukuCadang->harga}}</div>
+                        <div class="col-span-2 px-4">{{$penggantianSukuCadang->jumlah}}</div>
+                        <div class="col-span-4 px-4">{{$penggantianSukuCadang->getTotal()}}</div>
                     </div>
                 @endforeach
             </div>
 
             <div class="grid grid-cols-12 py-2 gap-4">
-                <div class="col-span-9 col-start-5 border-t-4 border-primary"></div>
-                <div class="col-span-4 col-start-5 uppercase font-bold text-xl">TOTAL SUKU CADANG</div>
-                <div class="col-span-4 col-start-9 text-xl">{{ $selectedService->getTotalPenggantianSukuCadangs()}}</div>
+                <div class="col-span-4 col-start-5 uppercase font-bold pl-4">TOTAL PENGGANTIAN</div>
+                <div class="col-span-4 col-start-9 pl-4">{{ $selectedService->getTotalPenggantianSukuCadangs()}}</div>
             </div>
         </div>
         <hr class="my-4">
+        @endif
 
         <div class="grid grid-cols-12 py-2 gap-4">
             <div class="col-span-12 border-t-8 border-primary"></div>
-            <div class="col-span-4 col-start-5 uppercase font-bold text-xl">Grandtotal</div>
-            <div class="col-span-4 col-start-9 text-xl">{{ $selectedService->getGrandTotal()}}</div>
+            <div class="col-span-4 col-start-5 uppercase font-bold text-xl pl-4">Grandtotal</div>
+            <div class="col-span-4 col-start-9 text-xl pl-4">{{ $selectedService->getGrandTotal()}}</div>
         </div>
 
     </x-card>
