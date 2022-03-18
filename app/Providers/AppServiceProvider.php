f9b4cc1e7747d185dc677bc59caadc6e998e4eca
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-03-18 10:28:38 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        URL::forceScheme('https');
 
         Builder::macro('search', function($field, $string){
             return $string ? $this->where($field, 'like', '%'.$string.'%') : $this;
