6f70dea0d3eb52b4ff5aec2716883724c9f3ecd0
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-03-22 14:16:24 +0700

    change mgiration for service

--- database/migrations/2022_03_11_063819_create_services_table.php
+++ database/migrations/2022_03_11_063819_create_services_table.php
@@ -14,14 +14,19 @@ class CreateServicesTable extends Migration
     public function up()
     {
         Schema::create('services', function (Blueprint $table) {
+            // $table->id();
+            // $table->string('no_plat');
+            // $table->foreignId('user_id')->constrained();
+            // $table->foreignId('pelanggan_id')->constrained();
+            // $table->date('tanggal');
+            // $table->string('keluhan')->nullable();
+            // $table->boolean('mau_diservice')->nullable();
+            // $table->boolean('service_selesai')->default(false);
+            // $table->timestamps();
             $table->id();
-            $table->string('no_plat');
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('pelanggan_id')->constrained();
-            $table->date('tanggal');
-            $table->string('keluhan')->nullable();
-            $table->boolean('mau_diservice')->nullable();
-            $table->boolean('service_selesai')->default(false);
+            $table->foreignId('pendaftaran_service_id')->constrained();
+            $table->dateTime('waktu_mulai');
+            $table->enum('status_service', ['cek', 'service', 'selesai'])->default('cek');
             $table->timestamps();
         });
     }
